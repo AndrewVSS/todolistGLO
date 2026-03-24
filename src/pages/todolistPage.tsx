3f0e6e6fbcc5642350f1626fc57afafb77b2f2e9
Ванчугов Андрей Сергеевич <vanchugov.a@SELECTEL.LOCL>
2026-03-24 19:54:48 +0300

    реализация состояния и кнопок

--- src/pages/todolistPage.tsx
+++ src/pages/todolistPage.tsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import { Form } from "../components/form/Form";
 import { Header } from "../components/header/Header";
-import { ToDoList } from "../components/todolist/ToDoList";
+import { TodoList } from "../components/todolist/ToDoList";
+import { ToDo } from "../models/todo-item";
 
 export const ToDoListPage = () => {
+  const [todos, setTodos] = useState<ToDo[]>([]);
+
+  const createNewToDo = (text: string) => {
+    const newToDo: ToDo = {
+      id: todos.length,
+      text: text,
+      isDone: false,
+    };
+    setTodos([...todos, newToDo]);
+  };
+
+  const updateToDo = (toDoItem: ToDo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === toDoItem.id) {
+        todo.isDone = !todo.isDone;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const deleteToDo = (toDoItem: ToDo) => {
+    const newTodos = todos.filter((todo) => todo.id !== toDoItem.id);
+    setTodos(newTodos);
+  };
+
   return (
     <>
       <Header />
-      <Form />
-      <ToDoList />
+      <Form createNewToDo={createNewToDo} />
+      <TodoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
     </>
   );
 };
